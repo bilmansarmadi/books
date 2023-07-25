9b68fa69285d295dfe71563bcbdb94e0fe917108
ASIATECH\bilma <bilmanbudiluhur@gmail.com>
2023-07-25 09:13:44 +0700

    menambahkan hhtp ngrok

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module OrderFood
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.hosts << "ad93-112-78-153-47.ngrok-free.app"
+    config.hosts << "0461-112-78-153-47.ngrok-free.app"
   end
 end
